6ee2bc3c3f067d85411cff6419b0bbcd99bf3278
Aleksei Smirnov <aleksei.smirnov@noveogroup.com>
2023-09-18 17:02:25 +0400

    Project with clear button issue reproduced

---
+++ TestClearButton/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestClearButton
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TestClearButton/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TestClearButton
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
